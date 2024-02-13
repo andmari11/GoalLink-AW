02aa899b70eb4a025508d3dd82cb9d3dca8ce3af
andmari11 <andres.mari.piqueras@gmail.com>
2024-02-13 20:05:20 +0100

    ejercicio1

---
+++ ejercicios/ej1_p10/saludos.php
@@ -0,0 +1,12 @@
+<?php
+    $vueltas = $_GET['num'];
+    echo '<h1>Me han pedido que te salude ' . $vueltas . ' veces.</h1>';
+    if ($vueltas == 42){
+        echo '<p>En realidad, esa es la respuesta a la pregunta final sobre la vida, el universo y todo lo demás.</p>';
+    }
+    else {
+        for ($i = 0; $i < $vueltas; $i++){
+            echo '<p>' . $i . ' - ¡Hola Mundo!</p>';
+        }
+    }
+?>  
