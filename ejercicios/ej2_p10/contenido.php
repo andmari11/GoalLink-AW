e820cd544b3873a6067b02b5a88ccd5a61ef1285
andmari11 <andres.mari.piqueras@gmail.com>
2024-02-14 10:46:07 +0100

    cambiosfoto

--- ejercicios/ej2_p10/contenido.php
+++ ejercicios/ej2_p10/contenido.php
@@ -32,17 +32,18 @@
 
                 echo "<h1>Contenido</h1>";
                 echo "
+                <h2>Citroën SM</h2>
+
                 <p>El Citroën SM es un automóvil de lujo y deportivo que fue producido por el fabricante francés Citroën entre 1970 y 1975. Es conocido por su diseño distintivo, su tecnología innovadora y su desempeño deportivo.</p>
                 
                 <p>Una de las características más destacadas del Citroën SM es su sistema de suspensión hidroneumática, que permitía un viaje suave y cómodo, así como un manejo excepcionalmente ágil. Esta suspensión ajustable también le permitía al conductor variar la altura del vehículo para adaptarse a diferentes condiciones de conducción.</p>
                 
+                <img src ='images.jpeg'>
+
+
                 <p>Además de su avanzada suspensión, el SM contaba con una dirección asistida hidráulica, frenos de disco en las cuatro ruedas y neumáticos radiales, todos ellos características de vanguardia en su época.</p>
-                
-                <p>El diseño del Citroën SM fue obra del renombrado diseñador italiano Robert Opron, quien logró crear un automóvil elegante y aerodinámico con líneas fluidas y una apariencia futurista. Su carrocería de dos puertas con techo tipo fastback y su amplio habitáculo lo convirtieron en un automóvil muy atractivo.</p>
-                
-                <p>Bajo el capó, el Citroën SM solía equiparse con un motor Maserati V6 de 2.7 litros o 3.0 litros, dependiendo del mercado, que ofrecía un rendimiento impresionante para su tiempo, con potencias que oscilaban entre los 170 y los 220 caballos de fuerza.</p>
-                
-                <img src ='logo.png'>";
+
+                ";
 
                 
             }
