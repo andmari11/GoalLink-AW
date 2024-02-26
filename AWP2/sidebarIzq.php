62b7a838b69ebbf8a1b8da07aef733a0d1d60c47
andmari11 <andres.mari.piqueras@gmail.com>
2024-02-26 11:42:48 +0100

    prueba

--- AWP2/sidebarIzq.php
+++ AWP2/sidebarIzq.php
@@ -1,9 +1,8 @@
 
 <nav id="sidebarIzq">
-		<h3>Navegación</h3>
 		<ul>
-			<li><a href="index.php">Inicio</a></li>
-			<li><a href="contenido.php">Ver contenido</a></li>
-			<li><a href="admin.php">Administrar</a></li>
+			<li><a href="index.php">Home</a></li>
+			<li><a href="contenido.php">Content</a></li>
+			<li><a href="foro.php">Foro</a></li>
 		</ul>
 </nav>
