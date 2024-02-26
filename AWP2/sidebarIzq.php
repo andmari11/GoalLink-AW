69d27bd07f7816e27bd3b8cd998bc83204c85b48
SergioSevilla22 <sergisev@ucm.es>
2024-02-26 18:37:55 +0100

    s

--- AWP2/sidebarIzq.php
+++ AWP2/sidebarIzq.php
@@ -1,12 +1,12 @@
 <?php
-   $sidebarIzq = <<<EOS
-		<nav id="sidebarIzq">
-				<ul>
-					<li><a href="index.php">Home</a></li>
-					<li><a href="contenido.php">Content</a></li>
-					<li><a href="foro.php">Foro</a></li>
-				</ul>
-		</nav>
-	EOS;
+    $sidebarIzq = <<<EOS
+    <nav id="sidebarIzq">
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="contenido.php">Content</a></li>
+            <li><a href="foro.php">Foro</a></li>
+        </ul>
+    </nav>
+    EOS;
 
 	require "esqueleto.php";
