2c184ade474ca73bf12f87aa2e8c05c7b86d2dd9
alvarm34 <alvarm34@ucm.es>
2024-02-27 11:30:06 +0100

    ahora si

--- AWP2/includes/comun/cabecera.php
+++ AWP2/includes/comun/cabecera.php
@@ -1,22 +1,19 @@
 <?php
-    function saludar(){
-        if(isset($_SESSION["login"])){
-            return "Bienvenido " . $_SESSION["nombre"] . " <a href='logout.php'> Logout</a>";
-        }
-        else{
-            return "Inicie sesión " . "<a href='login.php'>aquí</a>";
-        }
+function mostrarSaludo()
+{
+    if (isset($_SESSION["login"])) {
+        echo "<p>Bienvenido " . $_SESSION["nombre"] . "<a href = \"logout.php\">(Logout)</a></p>";
+    } else {
+        echo "<p>Usuario desconocido. <a href='login.php'>Login</a></p>";
     }
+}
+?>
 
-    $saludo = saludar();
-
-    $cabecera = <<<EOS
-    <header>
-        <h1><img src="img/logo.png" alt="logo" width="50" height="50">oalLink</h1>
-        <div class="saludo">
-            $saludo
-        </div>
-    </header>
-   EOS;
-
-   //require "esqueleto.php";
+<header>
+    <h1><img src="img/logo.png" alt="logo" width="50" height="50">oalLink</h1>
+    <div class="saludo">
+        <?php
+        mostrarSaludo();
+        ?>
+    </div>
+</header>
--- AWP2/includes/comun/sidebarIzq.php
+++ AWP2/includes/comun/sidebarIzq.php
@@ -1,12 +1,8 @@
-<?php
-    $sidebarIzq = <<<EOS
-    <nav id="sidebarIzq">
-        <ul>
-            <li><a href="index.php">Home</a></li>
-            <li><a href="contenido.php">Content</a></li>
-            <li><a href="foro.php">Foro</a></li>
-        </ul>
-    </nav>
-    EOS;
-
-	require("esqueleto.php");
+<nav id="sidebarIzq">
+    <h3>Navegación</h3>
+    <ul>
+        <li><a href="index.php">Inicio</a></li>
+        <li><a href="contenido.php">Ver contenido</a></li>
+        <li><a href="admin.php">Administrar</a></li>
+    </ul>
+</nav>
