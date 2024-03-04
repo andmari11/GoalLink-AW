22a86834af62d8c68973b0a0bb258b495b9c191e
alvarm34 <alvarm34@ucm.es>
2024-03-04 11:27:53 +0100

    barraizq

--- AWP2/index.php
+++ AWP2/index.php
@@ -3,7 +3,13 @@
 session_start();
 
 $titulo = 'Index';
-
+$barraIzq = <<<EOS
+<ul>
+<li><a href="index.php">Inicio</a></li>
+<li><a href="contenido.php">Ver contenido</a></li>
+<li><a href="admin.php">Admini</a></li>
+</ul>
+EOS;
 $contenido = <<<EOS
 <h1>Página principal</h1>
 <p> Aquí está el contenido público, visible para todos los usuarios. </p>
