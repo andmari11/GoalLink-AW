1d0f352c5abcb11b89709ac25639dba4876ed750
andmari11 <andres.mari.piqueras@gmail.com>
2024-03-25 22:54:26 +0100

    editar completado

--- AWP3/edit.php
+++ AWP3/edit.php
@@ -1,42 +1,22 @@
 <?php
-require "includes/model/usuarioModel.php";
+require_once __DIR__.'/includes/config.php';
 
-session_start();
+$formEditar = new \es\ucm\fdi\aw\usuarios\FormularioUsuarioEdit();
+$formEditar = $formEditar->gestiona();
 $titulo = 'Editar';
 
 
-if (isset($_SESSION["login"]) && $_SESSION["rol"] == 'a') {
+if (($app->usuarioLogueado()) && ($app->esAdmin())) {
 
-    $username= htmlspecialchars(trim(strip_tags($_REQUEST["usuario"])));
-
-    $usuario=Usuario::buscaUsuario($username);
-    $nombre=$usuario->getNombre();
-    $email=$usuario->getEmail();   
-     
-    $contenido = <<<EOS
-    <h2>Editar usuario</h2>
-    <form action="procesarEdit.php" method="post"> 
-        <fieldset>
-            <legend>Editar datos:</legend>
-            <label>Nombre:</label><input type="text" name="nombre" value="{$nombre}" required> 
-            <label>Email:</label><input type="text" name="email" value="{$email}" required> 
-            <label>Rol:</label> 
-            <select name="rol">
-                <option value="e">Editor</option>
-                <option value="m">Moderador</option>
-                <option value="b">Usuario</option>
-            </select>
-            <button type="submit">Siguiente</button>
-            <input type="hidden" name="nombreAntiguo" value="{$username}">
-        </fieldset>
-    </form>
-    
-EOS;
+    $contenido=<<<EOF
+    <h1>Acceso al sistema</h1>
+    $formEditar    
+    EOF;
 } else {
     $contenido = <<<EOS
     <h2>Acceso denegado</h2>
 EOS;
 }
 
-require __DIR__.'/includes/Vistas/esqueleto.php';
-
+$params = ['tituloPagina' => $titulo, 'contenidoPrincipal' => $contenido];
+$app->generaVista('/esqueleto2.php', $params);
